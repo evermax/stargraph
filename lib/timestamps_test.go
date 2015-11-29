6f67b15e12a8372d255a56c36b73a0bc0216df4d
bronzdoc <lsagastume1990@gmail.com>
2015-11-29 15:03:59 -0600

    Adding test for getStargazers

--- lib/timestamps_test.go
+++ lib/timestamps_test.go
@@ -45,7 +45,7 @@ func TestGetTimestampsSimple(t *testing.T) {
 	}
 	if !equals {
 		t.Fatalf("The expected timestamps %v and the actual ones %v"+
-			" don't have the sme values\n", expectedTimestamps, timestamps)
+			" don't have the same values\n", expectedTimestamps, timestamps)
 	}
 }
 
@@ -97,7 +97,7 @@ func TestGetTimestampsMulPages(t *testing.T) {
 	}
 	if !equals {
 		t.Fatalf("The expected timestamps %v and the actual ones %v"+
-			" don't have the sme values\n", expectedTimestamps, timestamps)
+			" don't have the same values\n", expectedTimestamps, timestamps)
 	}
 }
 
@@ -143,5 +143,45 @@ func TestGetTimestampsDistributed(t *testing.T) {
 	timestampCount := len(timestamps)
 	if timestampCount != expectedTimestamps {
 		t.Fatalf("Expected %d timestamps and got %d", expectedTimestamps, timestampCount)
+	}
+}
+
+func TestGetStargazers(t *testing.T) {
+	filePath := "testdata/simple_stars.json"
+	expectedTimestamps := []stargazer{{"2015-10-31T10:00:00Z"}, {"2015-10-31T11:00:00Z"}, {"2015-10-31T12:00:00Z"}}
+	body, err := ioutil.ReadFile(filePath)
+	batch := 1
+	if err != nil {
+		t.Fatalf("An error occured while reading the file %s: %v\n", filePath, err)
+	}
+
+	serverUrl := ""
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		linkFormat := BuildLinksFormat(serverUrl)
+		w.Header().Add("Link", fmt.Sprintf(linkFormat, 1, 1))
+		w.WriteHeader(http.StatusOK)
+		w.Write(body)
+	}
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	serverUrl = server.URL + "?per_page=" + strconv.Itoa(batch)
+
+	timestamps, _, err := getStargazers(server.URL, "token")
+	if err != nil {
+		t.Fatalf("An error occured while requesting the timestamps: %v\n", err)
+	}
+	if len(timestamps) != len(expectedTimestamps) {
+		t.Fatalf("The expected timestamps %v and the actual ones %v"+
+			" don't have the same size\n", expectedTimestamps, timestamps)
+	}
+	equals := true
+	for i, v := range timestamps {
+		if expectedTimestamps[i] != v {
+			equals = false
+			break
+		}
+	}
+	if !equals {
+		t.Fatalf("The expected timestamps %v and the actual ones %v"+
+			" don't have the same values\n", expectedTimestamps, timestamps)
 	}
 }
