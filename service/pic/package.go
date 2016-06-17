3491c0d6bf5e798718eefcac8bd1ba169a700102
Maxime <maxlasserre@free.fr>
2016-06-16 20:15:29 -0700

    Add the folder and explaination for two new services: user and pic

---
+++ service/pic/package.go
@@ -0,0 +1,4 @@
+// Package pic contains te code to be able to launch a picture service.
+// A picture service will serve as a proxy for the image delivery underlying service and map a repository
+// with it's associated star chart.
+package pic
---
+++ service/user/package.go
@@ -0,0 +1,3 @@
+// Package user contains the code to launch a user service
+// The user service is responsible for pulling up user data and serving it to the frontend.
+package user
